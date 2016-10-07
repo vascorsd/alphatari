1aaf4af000fbfd1abbfda786cd87be9bcebf50e3
Vasco Dias <m+git@vascorsd.com>
2016-10-08 00:40:10 +0100

    Try putting stuff into module.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,52 @@
+mod alphatari {
+    #[derive(Clone, Copy, Debug)]
+    pub enum Opt<T> {
+        None,
+        Some(T)
+    }
 
+    impl<T> Opt<T> {
+        pub fn is_empty(&self) -> bool {
+            match self {
+                &Opt::Some(_) => false,
+                &Opt::None => true
+            }
+        }
+
+        pub fn non_empty(&self) -> bool {
+            !self.is_empty()
+        }
+
+        pub fn fold<U, E, F>(self, if_empty: E, f: F) -> U
+            where E: FnOnce() -> U, F: FnOnce(T) -> U {
+            match self {
+                Opt::Some(x) => f(x),
+                Opt::None => if_empty()
+            }
+        }
+
+        pub fn get_or_else<F>(self, if_empty: F) -> T
+            where F: FnOnce() -> T {
+            self.fold(if_empty, free::identity)
+        }
+
+        pub fn map<U, F>(self, f: F) -> Opt<U>
+            where F: FnOnce(T) -> U {
+            self.fold(|| Opt::None, |x| Opt::Some(f(x)))
+        }
+
+        pub fn flat_map<U, F>(self, f: F) -> Opt<U>
+            where F: FnOnce(T) -> Opt<U> {
+            self.fold(|| Opt::None, f)
+        }
+    }
+
+    mod free {
+        pub fn identity<T>(x: T) -> T { x }
+    }
+}
+
+use alphatari::Opt;
 
 fn main() {
     // rust Option
@@ -13,7 +61,9 @@ fn main() {
 
     println!("{:?}", x2);
 
-    println!("{:?}", x1.get());
+    let y = x1.map(|x| println!("{:?}", x) );
+
+
     // my Opt
     // more scala styled.
     // ...
@@ -21,46 +71,3 @@ fn main() {
 }
 
 
-#[derive(Clone, Copy, Debug)]
-enum Opt<T> {
-    None,
-    Some(T)
-}
-
-impl<T> Opt<T> {
-    fn is_empty(&self) -> bool {
-        match self {
-            &Opt::Some(_) => false,
-            &Opt::None => true
-        }
-    }
-
-    fn non_empty(&self) -> bool {
-        !self.is_empty()
-    }
-
-    fn fold<U, E, F>(self, if_empty: E, f: F) -> U
-        where E: FnOnce() -> U, F: FnOnce(T) -> U {
-        match self {
-            Opt::Some(x) => f(x),
-            Opt::None => if_empty()
-        }
-    }
-
-    fn get_or_else<F>(self, if_empty: F) -> T
-        where F: FnOnce() -> T {
-        self.fold(if_empty, identity)
-    }
-
-    fn map<U, F>(self, f: F) -> Opt<U>
-        where F: FnOnce(T) -> U {
-        self.fold(|| Opt::None, |x| Opt::Some(f(x)))
-    }
-
-    fn flat_map<U, F>(self, f: F) -> Opt<U>
-        where F: FnOnce(T) -> Opt<U> {
-        self.fold(|| Opt::None, f)
-    }
-}
-
-fn identity<T>(x: T) -> T { x }
